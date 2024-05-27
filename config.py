c2193a120501f3bae6cea215c7f546c25db85b07
Glebkuz <153431235+Glebkuz@users.noreply.github.com>
2024-05-27 21:58:58 +0800

    work

---
+++ config.py
@@ -0,0 +1,21 @@
+
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+COUNT_LAST_MSG = 4
+
+LOGS = f'pythonProject8/logs.txt'  # файл для логов
+DB_FILE = f'pythonProject8/messages.db'  # файл для базы данных
+
+IAM_TOKEN = "t1.9euelZqXjJKQmMmcyMiLlZGNjI-YjO3rnpWazMjHm57Nx4ubjZuWiZOVnIzl9PdkTjhN-e8lFFTG3fT3JH01TfnvJRRUxs3n9euelZrIj8iUxpiVl4yejYqcx5aVk-_8xeuelZrIj8iUxpiVl4yejYqcx5aVk73rnpWax5DIyZzGxo-Nk5ecm5rPzZy13oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.bz5Y3_LiNyifM03TIPB14Pn2U6tbgMp5gR5lIalpty7BXe1BZs2qqTLLydRLA7nGNvClyvI__9NeUdRIgsanCg"
+FOLDER_ID = "b1gshjrk2cqqlcr257qj"
+BOT_TOKEN = '6570865512:AAH2wn9P-e1iXg2yMG6ieAMjrKydzOUoulw'
+
+MAX_USER_STT_BLOCKS = 20
+MAX_USER_TTS_SYMBOLS = 8_000
+MAX_USER_GPT_TOKENS = 8_000
+
+LOGS = 'logs.txt'
+DB_FILE = 'messages.db'
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
+                                            'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
+                                            'Изображай человека'}]
